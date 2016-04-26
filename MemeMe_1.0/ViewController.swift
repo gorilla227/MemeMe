7a6988fffcd6792416b98e7aa9981753f3aad708
Default <axu@qa-osx11-6095.corp.blizzard.net>
2016-04-26 10:20:59 -0700

    Initial Commit

---
+++ MemeMe_1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe_1.0
+//
+//  Created by Andy Xu on 16/4/26.
+//  Copyright © 2016年 Andy Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
